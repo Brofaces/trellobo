2649c6e2fc339fb3fac32aa3e191f5c052d994b1
Alex Chvatal <achvatal@redhat.com>
2014-03-25 15:49:26 -0400

    this is ruby, let's use blocks :D

--- utils.rb
+++ utils.rb
@@ -20,25 +20,29 @@ end
 
 # return an authenticated connection to the openshift mongo db
 def db_connect
-  db = MongoClient.new(ENV['OPENSHIFT_MONGODB_DB_HOST'], ENV['OPENSHIFT_MONGODB_DB_PORT']).db(ENV['OPENSHIFT_APP_NAME'])
+  con = MongoClient.new(ENV['OPENSHIFT_MONGODB_DB_HOST'], ENV['OPENSHIFT_MONGODB_DB_PORT'])
+  db = con.db(ENV['OPENSHIFT_APP_NAME'])
   db.authenticate(ENV['OPENSHIFT_MONGODB_DB_USERNAME'], ENV['OPENSHIFT_MONGODB_DB_PASSWORD'])
-  return db
+  yield db
+  con.close
 end
 
 # store a nick/login pair in the db
 def store_login(nick, login)
-  db = db_connect
-  # TODO: check to see if login is already stored. if so, update instead of insert
-  # TODO: close db connection after this is done!
-  db[$login_collection].insert({'_id' => nick, 'login' => login})
+  db_connect do |db|
+    # TODO: check to see if login is already stored. if so, update instead of insert
+    # TODO: close db connection after this is done!
+    db[$login_collection].insert({'_id' => nick, 'login' => login})
+  end
 end
 
 # given a nick, grab the stored trello login from the db
 def get_login(nick)
   # TODO: close db connection after this is done!
-  db = db_connect
-  doc = db[$login_collection].find_one({'_id' => nick_parse(nick)})
-  doc[$login_collection] if doc
+  db_connect do |db|
+    doc = db[$login_collection].find_one({'_id' => nick_parse(nick)})
+    doc[$login_collection] if doc
+  end
 end
 
 # check the db to see if the listed members have lonely cards that need updating
